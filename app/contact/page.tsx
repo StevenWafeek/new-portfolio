ebd225f9dddc7ac995bed898fa8991e3567c2cb7
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2024-03-02 08:47:24 +0200

    Update contact me page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import styles from "../page.module.css";
 import Discription from "@/components/discription";
 
@@ -7,7 +8,45 @@ return(
               <Discription />
     </div>
     <div>
-        <h1>here is contact page ...</h1>
+    <div>
+      <h2 className="contact-header">Contact me</h2>
+      <p>
+        If you have an application you are interested in developing, a feature
+        that you need built or a project that needs coding. I'd love to help
+        with it!
+      </p>
+      <div className="contact-form">
+        <form action="https://formspree.io/f/mlekpene" method="post" className="form">
+          <input
+            className="name"
+            type="text"
+            maxLength={30}
+            placeholder="Lorem Ipsum"
+            required
+            name="name"
+          />
+          <input
+            className="email"
+            type="email"
+            name="email"
+            placeholder="loremipsum@gmail.com"
+            required
+          />
+          <textarea
+            className="textarea"
+            name="massage"
+            cols={30}
+            rows={10}
+            maxLength={500}
+            required
+            placeholder="Write your message here..."
+          ></textarea>
+          <button type="submit" className="form-button">
+            Get in touch
+          </button>
+        </form>
+      </div>
+    </div>
     </div>
 </>
 )
