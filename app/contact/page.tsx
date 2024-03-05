1c4fdaa2f3df573324b28d04cf4d7e69c3c19632
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2024-03-05 11:05:16 +0200

    Update page contact me

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,20 +1,43 @@
 /* eslint-disable react/no-unescaped-entities */
 import styles from "../page.module.css";
 import Discription from "@/components/discription";
+import { FaTwitter, FaLinkedinIn, FaGithub, FaHandPeace } from 'react-icons/fa';
+import './contact.css';
 
 function Contact (){
 return(
     <><div className={styles.main}>
               <Discription />
     </div>
-    <div>
-    <div>
-      <h2 className="contact-header">Contact me</h2>
-      <p>
-        If you have an application you are interested in developing, a feature
-        that you need built or a project that needs coding. I'd love to help
-        with it!
-      </p>
+    <div className="contact-container">
+    <ul className="Icon">
+      <li>
+        <a href="https://twitter.com/steven_hakeem" target="_blank" rel="noopener">
+          <FaTwitter />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/steven-wafeek-b629341b6/" target="_blank" rel="noopener">
+          <FaLinkedinIn />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/StevenWafeek" target="_blank" rel="noopener">
+          <FaGithub />
+        </a>
+      </li>
+      <li>
+        <a href="https://wellfound.com/u/steven-wafeek" target="_blank" rel="noopener">
+          <FaHandPeace />
+        </a>
+      </li>
+
+    </ul>
+      <button id="resume">
+        <a href="https://docs.google.com/document/d/11cu9r1p6hKyq3m0sKOkr90dkIvyh4Rjx-XhA_C4af8A/edit" target="_blank" rel="noopener">Get my resume</a>
+      </button>
+    <div className="contactme">
+      <h2 className="contact-header">CONTACT ME</h2>
       <div className="contact-form">
         <form action="https://formspree.io/f/mlekpene" method="post" className="form">
           <input
@@ -45,6 +68,20 @@ return(
             Get in touch
           </button>
         </form>
+        <div className="contact-info">
+          <p>
+            If you have an application you are interested in developing, a feature
+            that you need built or a project that needs coding. I'd love to help
+            with it!
+          </p>
+          <h2>Email</h2>
+          <p>steven.wafeek@gmail.com</p>
+          <h2>Phone</h2>
+          <p>+2 01271850404</p>
+          <h2>Adress</h2>
+          <p>shar3 el gesh street</p>
+
+        </div>
       </div>
     </div>
     </div>
